3f7264fcffcc4bfa2f3636c84aa9081e45c7e785
santilazzari <santilazzari1994@gmail.com>
2016-06-13 19:21:49 -0300

    Added limit iterations to perceptron... todo : Parse training set and use a perceptron, the make the neural network

--- perceptron.py
+++ perceptron.py
@@ -2,7 +2,7 @@
 class Perceptron(object):
     wights = []
     threshold = 0
-    learningRate = 0
+    learningRate = 0;
 
 
     def __init__(self, dimension, threshold, learningRate):
@@ -13,10 +13,12 @@ class Perceptron(object):
     def dotProduct(self, values):
         return sum(valor * weight for valor, weight in zip(values, self.weights))
 
-    def train(self, trainingSet):
+    def train(self, trainingSet, limitIterations):
         while True:
             print('-' * 60)
             errorSet = 0
+            limitIterations -= 1
+
             for vector_de_entrada, salida_deseada in trainingSet:
                 print(self.weights)
                 result = self.dotProduct(vector_de_entrada) > self.threshold
@@ -25,21 +27,26 @@ class Perceptron(object):
                     errorSet += 1
                     for indice, valor in enumerate(vector_de_entrada):
                         self.weights[indice] += self.learningRate * error * valor
-            if errorSet == 0:
+            if (errorSet == 0) or (limitIterations == 0):
                 break
         return
 
-    def test():
-        #traing data
-        return
+    def test(self, inputValue):
+        return (self.dotProduct(inputValue) - self.threshold) > 0
+
 
 def test():
     threshold = 0.5
     leaarningRate = 0.1
     pesos = [0, 0, 0]
     trainingSet = [((1, 0, 0), 1), ((1, 0, 1), 1), ((1, 1, 0), 1), ((1, 1, 1), 0)]
+    limitIterations = 100000
 
     perceptron = Perceptron(3, threshold, leaarningRate)
-    perceptron.train(trainingSet)
+    perceptron.train(trainingSet, limitIterations)
+
+    print perceptron.test((0, 1, 0))
+
+
 
 test()
